27c486c16a8e59d27cffed8ef9369486e4051eb1
BrianGoodrich <bgoodrich201@gmail.com>
2023-04-16 16:47:31 -0500

    added input boxes and button with loading status

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,19 +1,69 @@
+import Col from 'react-bootstrap/Col';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
 require("./InputBox.css")
 
 function InputBox() {
-  return (
+    function simulateNetworkRequest() {
+        return new Promise((resolve) => setTimeout(resolve, 500));
+      }
+      
+      function LoadingButton() {
+        const [isLoading, setLoading] = useState(false);
+      
+        useEffect(() => {
+          if (isLoading) {
+            simulateNetworkRequest().then(() => {
+              setLoading(false);
+            });
+          }
+        }, [isLoading]);
+      
+        const handleClick = () => setLoading(true);
+      
+        return (
+          <Button
+            variant="primary"
+            disabled={isLoading}
+            onClick={!isLoading ? handleClick : null}
+            style={{float: "right", marginTop: "10px"}}
+          >
+            {isLoading ? 'Loading…' : 'Click to load'}
+          </Button>
+        );
+      }
 
+
+
+  return (
+    
     
     <div class = "inputWrapper">
-        <FloatingLabel controlId="floatingInput" label="First Name" className="mb-3">
-            <Form.Control type="text" placeholder="First" />
+        <Row className="g-2">
+        <p class = "readyText">Are you ready to claim your number?</p>
+      <Col md>
+        <FloatingLabel controlId="floatingInputGrid" label="Please enter your name" style = {{width : "500px", margin: "2px"}}>
+          <Form.Control type="text"/>
         </FloatingLabel>
-        <FloatingLabel controlId="floatingInput" label="Last Name" width = "">
-            <Form.Control type="text" placeholder="Last"/>
+      </Col>
+    </Row>
+    <Row className="g-2">
+      <Col md>
+        <FloatingLabel controlId="floatingInputGrid" label="Your Number (Hint: This box is very long for a reason!)" style={{margin: "2px", width: "185%"}}>
+          <Form.Control type="text"/>
         </FloatingLabel>
+      </Col>
+      <Col md>
+      <LoadingButton/>
+      </Col>
+      
+    </Row>
+    
+    
     </div>
     
   );
