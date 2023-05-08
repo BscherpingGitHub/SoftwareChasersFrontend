cfe9896eebf068836ab17d3d119d01d2ec69b1a8
BrianGoodrich <bgoodrich201@gmail.com>
2023-05-07 22:18:00 -0500

    trying broswer router with basename for gh pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {HashRouter,BrowserRouter} from "react-router-dom";
+import {HashRouter, BrowserRouter} from "react-router-dom";
 require("./index.css");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
-      <HashRouter>
+    <BrowserRouter basename="/SoftwareChasersFrontend">
       <App />
-      </HashRouter>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
