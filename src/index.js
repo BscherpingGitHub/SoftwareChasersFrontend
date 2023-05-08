741d13ccdab2d342f0986b3bb54fbecfdd7beac4
BrianGoodrich <bgoodrich201@gmail.com>
2023-05-07 20:15:43 -0500

    fixing package.json for gh pages config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, HashRouter as Router } from "react-router-dom";
+import { BrowserRouter} from "react-router-dom";
 require("./index.css");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
