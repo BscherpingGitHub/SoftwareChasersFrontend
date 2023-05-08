0e0052dba263e9a4006482ddcb9540fd6bf62898
BrianGoodrich <bgoodrich201@gmail.com>
2023-05-07 19:58:56 -0500

    added router in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 require("./index.css");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
